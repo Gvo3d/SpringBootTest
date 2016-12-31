b409a405fa44b4fe5f80e77e8585acbe230739ba
Gvo3d <denis.yakimov89@gmail.com>
2016-12-31 12:36:44 +0200

    Spring Boot Web init - v0.4

---
+++ SpringBootTest/src/main/java/projectpackage/configuration/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package projectpackage.configuration;
+
+/**
+ * Created by Gvozd on 31.12.2016.
+ */
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll();
+    }
+}
