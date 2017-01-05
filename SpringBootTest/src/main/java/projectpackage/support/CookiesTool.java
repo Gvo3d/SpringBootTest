d496cb285278b693c4d69fd18cb038bd92e5eb18
Gvo3d <denis.yakimov89@gmail.com>
2017-01-05 22:40:39 +0200

    Spring Boot Web init - v0.22 CHANGES: *InterMessages from database(Template for properties mesage source). *CookiesTool for managing cookies
    TODO: Add a proper user model, add a repository for it and service-layer for security purposes, add error403 page and config context
    
    TRYING: working with ThyMeLeaf

--- SpringBootTest/src/main/java/projectpackage/support/CookiesTool.java
+++ SpringBootTest/src/main/java/projectpackage/support/CookiesTool.java
@@ -1,7 +1,37 @@
 package projectpackage.support;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by Gvozd on 01.01.2017.
  */
 public class CookiesTool {
+    public static String searchCookie(String cookieName, HttpServletRequest request) {
+        return getCookie(cookieName, request);
+    }
+
+    public static boolean aquireCookie(String cookieName, HttpServletRequest request) {
+        if (null!=getCookie(cookieName, request)) {
+            return true;
+        } else return false;
+    }
+
+    private static String getCookie(String cookieName, HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookieName.equals(cookie.getName())) {
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
+
+    public static void setCookie(String cookieName, String value, int maxAge, HttpServletResponse response) {
+        Cookie cookie = new Cookie(cookieName, value);
+        cookie.setMaxAge(maxAge);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
 }
